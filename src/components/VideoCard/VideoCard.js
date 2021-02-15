9b9169b1fa43f70838eed5cc254663c2e2899271
taurusilver7 <prasad20697@gmail.com>
2021-02-15 22:03:45 +0530

    videocard component added

---
+++ src/components/VideoCard/VideoCard.js
@@ -0,0 +1,34 @@
+import React, { forwardRef } from "react";
+import "./VideoCard.css";
+
+import TextTruncate from "react-text-truncate";
+import { ThumbUpSharp } from "@material-ui/icons";
+
+const baseURL = "https://image.tmdb.org/t/p/original/";
+
+const VideoCard = forwardRef(({ movie }, ref) => {
+  return (
+    <div ref={ref} className="videoCard">
+      <img
+        src={`${baseURL}${movie?.backdrop_path || movie?.poster_path}`}
+        alt="poster"
+      />
+
+      <TextTruncate
+        line={1}
+        element="p"
+        truncateText="..."
+        text={movie?.overview}
+      />
+      <h2>{movie?.original_title || movie?.title}</h2>
+      <p className="videoCard__stats">
+        {movie?.media_type && `${movie?.media_type} ·`}
+        {movie?.release_date || movie?.first_air_date}{" "}
+        <ThumbUpSharp />{" "}
+        {movie?.vote_count}
+      </p>
+    </div>
+  );
+});
+
+export default VideoCard;
