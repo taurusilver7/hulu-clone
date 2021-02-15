67b706f957d1684eb08ec14239a71b6b5da9f4c4
taurusilver7 <prasad20697@gmail.com>
2021-02-15 22:01:57 +0530

    navbar component added

---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,38 @@
+import React from "react";
+import "./Navbar.css";
+
+import requests from "../../requests";
+
+const Navbar = ({ setSelectedOption }) => {
+  return (
+    <div className="navbar">
+      <h2 onClick={() => setSelectedOption(requests?.fetchTrending)}>
+        Trending
+      </h2>
+      <h2 onClick={() => setSelectedOption(requests?.fetchTopRated)}>
+        Top rated
+      </h2>
+      <h2 onClick={() => setSelectedOption(requests?.fetchActionMovies)}>
+        Action
+      </h2>
+      <h2 onClick={() => setSelectedOption(requests?.fetchComedyMovies)}>
+        Comedy
+      </h2>
+      <h2 onClick={() => setSelectedOption(requests?.fetchRomanceMovies)}>
+        Romance
+      </h2>
+      <h2 onClick={() => setSelectedOption(requests?.fetchHorrorMovies)}>
+        Horror
+      </h2>
+      <h2 onClick={() => setSelectedOption(requests?.fetchMystery)}>Mystery</h2>
+      <h2 onClick={() => setSelectedOption(requests?.fetchSciFi)}>Sci fi</h2>
+      <h2 onClick={() => setSelectedOption(requests?.fetchWestern)}>Western</h2>
+      <h2 onClick={() => setSelectedOption(requests?.fetchAnimation)}>
+        Animation
+      </h2>
+      <h2 onClick={() => setSelectedOption(requests?.fetchTv)}>Movie</h2>
+    </div>
+  );
+};
+
+export default Navbar;
